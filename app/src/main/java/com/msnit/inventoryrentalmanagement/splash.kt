ed867b95bfde95531d42787ccdb744a31cfa7a68
kali <mona21820019@gmail.com>
2023-08-30 13:02:01 +0300

    3 pages done

--- app/src/main/java/com/msnit/inventoryrentalmanagement/splash.kt
+++ app/src/main/java/com/msnit/inventoryrentalmanagement/splash.kt
@@ -4,18 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class splash : AppCompatActivity() {
     private val delayDuration: Long = 6000 // 5 seconds
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
         val handler = android.os.Handler()
 
         handler.postDelayed({
             // تنفيذ الانتقال إلى الواجهة الأخرى هنا
-            val intent = Intent(this, splash::class.java)
+            val intent = Intent(this, signup::class.java)
             startActivity(intent)
             finish() // اختياري: إغلاق الواجهة الحالية إذا لم تكن بحاجة إليها
         }, delayDuration)
